ad37032fb6dc76b754f855b75d70c4af0b60e188
skinny4botan <ermolaevtimofej15@gmail.com>
2026-04-22 08:21:58 +0400

    Create server.py

---
+++ server.py
@@ -0,0 +1,18 @@
+import os
+import telebot
+from telebot.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+TOKEN = os.environ.get('BOT_TOKEN')
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    first_name = message.from_user.first_name or "Guest"
+    text = f"Welcome, {first_name}! This is the place where you'll find all the trending games — and more🔥\n\n✅Subscribe to the channel and unlock full access to games👇"
+    keyboard = InlineKeyboardMarkup()
+    button = InlineKeyboardButton(text="SUBSCRIBE", url="https://clicks.su/m2bGNq")
+    keyboard.add(button)
+    bot.send_message(message.chat.id, text, reply_markup=keyboard)
+
+print("✅ Бот Victoria Club Bot запущен!")
+bot.infinity_polling()
